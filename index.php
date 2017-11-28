8268823608bd75c8f6bfef144ae875409ab074fe
Seul Lee <27seul.lee@gmail.com>
2017-11-28 01:17:35 -0500

    merging in process

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <title>inSource</title>
 
   <!-- css -->
-  <link rel="stylesheet" href="http://localhost:8080/inSource/css/master.css">
+  <link rel="stylesheet" href="/css/master.css">
 
   <!-- font -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,600" rel="stylesheet">
@@ -26,8 +26,8 @@
 <body>
   <header id="split-nav">
     <div class="branding">
-      <a href="http://localhost:8080/inSource/index.php">
-      <img class="logo" src="http://localhost:8080/inSource/assets/logo_inSource.png" alt="inSource logo" /></a>
+      <a href="/index.php">
+      <img class="logo" src="/assets/logo_inSource.png" alt="inSource logo" /></a>
     </div>
     <nav>
       <ul>
@@ -56,7 +56,7 @@
   <footer>
     <div class="footer-content">
       <p><i class="fa fa-copyright" aria-hidden="true"></i> 2017
-      <img class="footer-logo" src="http://localhost:8080/inSource/assets/logo_inSource.png" alt="inSource logo"></p>
+      <img class="footer-logo" src="/assets/logo_inSource.png" alt="inSource logo"></p>
     </div>
   </footer>
 </body>
