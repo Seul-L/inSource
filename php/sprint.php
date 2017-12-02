ade218f2909cbd4dbf9de2f19dbaf0d053b5b889
thisisoke <seyitanoke@gmail.com>
2017-12-01 23:06:20 -0500

    Added Sprint.php

---
+++ php/sprint.php
@@ -0,0 +1,18 @@
+<!-- vote phase -->
+<section class="sprint">
+  <h2>WORKSHOP DETAILS</h2>
+  <h3 class="red">4 days left to RSVP for Sprint Workshop</h3>
+  <h3 class="red">Workshop will take place on December 10th, 2017</h3>
+
+  <p>After a topic has been determined, a random selection of participants (operators and management alike) will be invited to an InSource Sprint Workshop aimed at developing a solution to the proposed problem. This Sprint Workshop will be facilitated by an InSource Sprint Master who will act as a neutral party. The structured Sprint Workshop will guide participants through specific exercises that will help to develop a viable solution</p>
+
+  <h2>TOPIC CHOSEN:</h2>
+
+  <
+<h4 class="label-non-select">How might we improve driver facilities such as lunch rooms or break rooms?</h4>
+
+
+  <script type="text/javascript">
+
+  </script>
+</section>
